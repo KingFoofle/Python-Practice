c92a52f16d6376a7dd05cf5104613135afe72d0b
KingFoofle <janngarciapagan@gmail.com>
2020-03-10 20:57:32 -0400

    Added base to create multiple problems. Problem 2 to come soon

---
+++ ProblemBase/Problem.py
@@ -0,0 +1,23 @@
+import test_Problem
+
+
+# Problem:
+# TODO: Make problem
+
+# ------- DO NOT EDIT THE CODE BELOW -------
+
+def test_function_1():
+    """Copy/Paste For each exercise in the problem"""
+    try:
+        test_Problem.Test().test_problem()
+
+    except AssertionError as e:
+        print(e)
+        print("Your code must pass all tests to succeed. Try again!")
+
+    else:
+        print("Function 1 Passed")
+
+
+if __name__ == "__main__":
+    test_function_1()
---
+++ ProblemBase/test_Problem.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+import Problem
+
+
+class Test(TestCase):
+    def test_problem(self):
+        self.fail("Tests not implemented yet")
